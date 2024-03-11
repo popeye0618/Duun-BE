25da07ff9ae1abf1268fb8ec92336a543c0c6d96
H_JU_0527 <143387515+kim0527@users.noreply.github.com>
2024-03-11 14:15:54 +0900

    ProjectFileDefault

---
+++ src/main/java/dunn/dunnshop/DunnShopApplication.java
@@ -0,0 +1,13 @@
+package dunn.dunnshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DunnShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DunnShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dunn/dunnshop/DunnShopApplicationTests.java
@@ -0,0 +1,13 @@
+package dunn.dunnshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DunnShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
